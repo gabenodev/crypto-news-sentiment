2cc87eded17d9ddcf2930625532ad865d39fd177
gabenodev <hiimgabigp@gmail.com>
2025-03-03 23:34:37 +0200

    Added search bar
    News, AltcoinSeason Index Sentiment Trend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-// src/App.js
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
-import SentimentChart from "./components/SentimentChart"; // SentimentChart rămâne aici
-import NewsCard from "./components/NewsCard"; // Importăm NewsCard pentru a-l folosi
+import SentimentChart from "./components/SentimentChart";
+import NewsCard from "./components/NewsCard";
 import AltcoinSeason from "./components/AltcoinSeason";
 import Footer from "./components/Footer";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("news"); // Setăm tab-ul activ, default este "news"
+
   return (
     <div className="App bg-gray-100 min-h-screen">
-      <Header />
+      <Header setActiveTab={setActiveTab} />{" "}
+      {/* Transmitere funcție setActiveTab către Header */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="space-y-12">
-          {" "}
-          {/* Adăugăm spațiere uniformă între componente */}
-          <SentimentChart />
-          <AltcoinSeason />
-          <NewsCard />
+          {/* Afișează componenta corespunzătoare bazat pe tab-ul activ */}
+          {activeTab === "sentiment" && <SentimentChart />}
+          {activeTab === "altcoin" && <AltcoinSeason />}
+          {activeTab === "news" && <NewsCard />}
         </div>
       </main>
       <Footer />
