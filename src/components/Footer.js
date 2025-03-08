d72b5162cbc09136624c2343e60c238b8f3da5d8
gabenodev <hiimgabigp@gmail.com>
2025-03-08 21:04:43 +0200

    updated header UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-blue-600 to-indigo-700 text-white py-8 text-center">
+    <footer className="bg-gradient-to-r from-gray-800 to-gray-900 text-white py-8 text-center">
       <p className="text-lg">
         &copy; 2025 Crypto Sentiment. All rights reserved.
       </p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import NightToggle from "./NightToggle"; // Importă componenta NightToggle
 
 function Header({ setActiveTab }) {
   return (
-    <header className="bg-gradient-to-r from-blue-600 to-indigo-700 text-white py-6 shadow-lg">
+    <header className="bg-gradient-to-r from-gray-800 to-gray-900 text-white py-6 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center">
+        <div className="flex items-center w-full">
           {/* Title */}
-          <h1 className="text-4xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-green-400 dark:text-white">
-            Crypto News and Sentiment Analysis
+          <h1 className="text-3xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-green-400 text-left w-full">
+            SentimentX
           </h1>
 
           {/* Night Toggle */}
@@ -19,29 +19,28 @@ function Header({ setActiveTab }) {
         <nav className="mt-6 flex justify-center space-x-8 text-lg font-semibold">
           <button
             className="text-white hover:text-teal-300 transition duration-300"
-            onClick={() => setActiveTab("news")} // Actualizează tab-ul activ la "news"
+            onClick={() => setActiveTab("news")}
           >
             News
           </button>
 
           <button
             className="text-white hover:text-teal-300 transition duration-300"
-            onClick={() => setActiveTab("altcoin")} // Actualizează tab-ul activ la "altcoin"
+            onClick={() => setActiveTab("altcoin")}
           >
             AltcoinSeason Index
           </button>
 
           <button
             className="text-white hover:text-teal-300 transition duration-300"
-            onClick={() => setActiveTab("sentiment")} // Actualizează tab-ul activ la "sentiment"
+            onClick={() => setActiveTab("sentiment")}
           >
             Sentiment Trend
           </button>
 
-          {/* Adăugăm butonul pentru Whale Transactions */}
           <button
             className="text-white hover:text-teal-300 transition duration-300"
-            onClick={() => setActiveTab("whaleTransactions")} // Actualizează tab-ul activ la "whaleTransactions"
+            onClick={() => setActiveTab("whaleTransactions")}
           >
             Whale Transactions
           </button>
