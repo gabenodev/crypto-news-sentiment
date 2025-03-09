209bacd31dee659257822a36f87151c9a59c4ff0
gabenodev <hiimgabigp@gmail.com>
2025-03-09 16:25:15 +0200

    Eliminated motion import
    Motion import not used

--- src/components/AltcoinSeason.js
+++ src/components/AltcoinSeason.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import AltcoinChart from "./AltcoinChart";
 
 const AltcoinSeason = () => {
